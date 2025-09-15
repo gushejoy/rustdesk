6b4711fc9e0fe44a7595fce4b02acc02ab0ca87d
gush <414165181@qq.com>
2025-09-15 16:37:25 +0800

    Desktop 被连接时候 提示窗口自动最小化

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -601,10 +601,10 @@ class ServerModel with ChangeNotifier {
     });
     // Only do the hidden task when on Desktop.
     if (client.authorized && isDesktop) {
-      cmHiddenTimer = Timer(const Duration(seconds: 3), () {
+      // cmHiddenTimer = Timer(const Duration(seconds: 3), () {
         if (!hideCm) windowManager.minimize();
-        cmHiddenTimer = null;
-      });
+        // cmHiddenTimer = null;
+      // });
     }
     parent.target?.chatModel
         .updateConnIdOfKey(MessageKey(client.peerId, client.id));

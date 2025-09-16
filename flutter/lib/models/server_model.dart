1fdb2cc267e1f013a0b1065219b4b0fa03e561fa
gush <414165181@qq.com>
2025-09-16 17:29:29 +0800

    m

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -601,7 +601,7 @@ class ServerModel with ChangeNotifier {
     });
     // Only do the hidden task when on Desktop.
     if (client.authorized && isDesktop) {
-      cmHiddenTimer = Timer(const Duration(milliseconds: 100), () {
+      cmHiddenTimer = Timer(const Duration(milliseconds: 50), () {
         if (!hideCm) windowManager.minimize();
         cmHiddenTimer = null;
       });
